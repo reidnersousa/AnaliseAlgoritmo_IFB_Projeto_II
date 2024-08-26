4c4a6162a2a0152ae798f59e851a5ec05d56510f
reidner_sousa <79096521+reidnersousa@users.noreply.github.com>
2024-08-26 16:28:05 -0300

    Add files via upload

---
+++ Projeto II/Projeto II/bubble_sort.py
@@ -0,0 +1,87 @@
+import numpy as np
+import math
+
+def encontrar_indice(numero, lista):
+    try:
+        indice = lista.index(numero)
+        return indice
+    except ValueError:
+        return None
+
+def gerando_amostra(N):
+    sequencia = np.random.randint(1, N + 1, N)
+    return sequencia
+
+def eh_multiplo(N, qtd_partes):
+    return N % qtd_partes == 0
+
+def bubbleSort(arr):
+    arr = arr.copy()  # Cria uma cópia para não alterar a lista original
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+    return arr
+
+def etapa_1(vetor):
+    tam_vetor = len(vetor)
+    qtd_partes = int(np.sqrt(tam_vetor))
+    lista = []
+    j = 0
+    for i in range(0, qtd_partes):
+        j += 1
+        corte = vetor[qtd_partes * i:qtd_partes * j]
+        corte = bubbleSort(corte)
+        lista.append(corte)
+    
+    if j * qtd_partes < tam_vetor:
+        ultimo_corte = vetor[j * qtd_partes:]
+        lista.append(ultimo_corte)
+    
+    return lista
+
+def sort_pedacos(vetor):
+    lista = []
+    for i in vetor:
+        lista.append(bubbleSort(i))
+    return lista
+
+def etapa_2(vetor):
+    vetor_solucao = []
+    lista_maiores = []
+    for i in vetor:
+        maior_elemento = i[-1]
+        lista_maiores.append(maior_elemento)
+    
+    lista_maiores_ordenados = bubbleSort(lista_maiores).copy()
+    maior = etapa_3(lista_maiores_ordenados)
+    indice = encontrar_indice(maior, lista_maiores)
+
+    vetor[indice] = np.delete(vetor[indice], -1)
+    vetor = [arr for arr in vetor if arr.size > 0]
+    vetor_solucao.append(maior)
+    return vetor_solucao, vetor
+
+def etapa_3(lista_maiores):
+    maior_ele = lista_maiores[-1]
+    return maior_ele
+
+def etapa_4(vetor):
+    vetor = etapa_1(vetor)
+    vetor = sort_pedacos(vetor)
+    #print("input\n\n", vetor)
+    vetor_solucao = []
+    while len(vetor) > 0:
+        solucao_parcial, vetor = etapa_2(vetor)
+        vetor_solucao.append(solucao_parcial[0])
+
+    return vetor_solucao
+
+N = 10**4
+vetor = gerando_amostra(N)
+
+v_s = etapa_4(vetor)
+v_s_convertido = [int(i) for i in v_s]
+#print("\n\n",v_s_convertido)
+
--- Projeto II/heap_sort.py
+++ Projeto II/heap_sort.py
@@ -64,41 +64,52 @@ def print_heap(vetor):
         
 def etapa_2(vetor):
     maiores_2 = []
+    
     vetor = [heap for heap in vetor if len(heap.GETHEAP()) > 0]
-
+    print_heap(vetor)
+   
     # Remover o maior elemento de cada heap e armazenar em maiores_2
     for heap in vetor:
         maiores_2.append(heap.REMOVEHEAP())
     
-    print(maiores_2)
+    #print(maiores_2)
     
     # Criar uma nova heap com os maiores elementos removidos
     nova_heap = Heap(maiores_2)
-    print(nova_heap.GETHEAP())
+    #print(nova_heap.GETHEAP())
     
     # Remover o maior elemento da nova heap
     m = nova_heap.REMOVEHEAP()
-    print("m", m, "heap", nova_heap.GETHEAP())
+    #print("m", m, "heap", nova_heap.GETHEAP())
     
     # Inserir os valores de volta nas heaps originais
     for i, heap in enumerate(vetor):
         if i < len(maiores_2):
-            print("Heap antes:", heap.GETHEAP())
+            #print("Heap antes:", heap.GETHEAP())
             heap.INSERTHEAP(maiores_2[i])
-            print("Heap depois:", heap.GETHEAP(), "i", i, "m", maiores_2[i])
+            #print("Heap depois:", heap.GETHEAP(), "i", i, "m", maiores_2[i])
         else:
             break
-    return m 
+    return m ,vetor
+def etapa_4(vetor):
+    vetor_solucao = []
+    while  len(vetor)> 0:
+       
+        vs,vetor=etapa_2(vetor)
+        if vs is not None:
+            vetor_solucao.append(vs)
     
-
-N = 10**1
+    return vetor_solucao
+N = 5**1
 vetor = gerando_amostra(N)
 #print(vetor)
+
 vetor_solucao = []
 vetor=etapa_1(vetor)
-##
-print_heap(vetor)
-m=etapa_2(vetor)
-vetor_solucao.append(m)
+#etapa_4(vetor)
+#m=etapa_2(vetor)
+#vetor_solucao.append(m)
+#print(vetor_solucao)
+vetor_solucao=etapa_4(vetor)
 print(vetor_solucao)
-###
+
